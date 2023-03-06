4afd0192a9704e803c6d60e0b1ff7b3b03ac19ef
Christoph <christoph.saile@googlemail.com>
2023-03-06 21:15:42 +0100

    feat: update styles

--- src/components/my-card/my-card.tsx
+++ src/components/my-card/my-card.tsx
@@ -1,47 +1,54 @@
 import { Component, Host, h } from '@stencil/core';
-import { Build } from '@stencil/core';
-
 @Component({
   tag: 'my-card',
   styleUrl: 'my-card.css',
   shadow: true,
 })
 export class MyCard {
-  private wrapper: HTMLElement;
+  // private wrapper: HTMLElement;
 
-  private resizeObserver: ResizeObserver;
+  // private resizeObserver = new ResizeObserver(([entry]) => {
+  //   const { width } = entry.contentRect;
+  //   if (width < 600) {
+  //     this.wrapper.style.flexDirection = 'column';
+  //   } else {
+  //     this.wrapper.style.flexDirection = 'row';
+  //   }
+  // });
 
-  componentDidLoad() {
-    if (Build.isBrowser) {
-      this.resizeObserver = new ResizeObserver(([entry]) => {
-        const { width } = entry.contentRect;
-        if (width < 400) {
-          this.wrapper.style.flexDirection = 'column';
-        } else {
-          this.wrapper.style.flexDirection = 'row';
-        }
-      });
-      this.resizeObserver.observe(this.wrapper);
-    }
-  }
+  // componentDidLoad() {
+  //   this.resizeObserver.observe(this.wrapper);
+  // }
 
-  disconnectedCallback() {
-    this.resizeObserver.disconnect();
-  }
+  // disconnectedCallback() {
+  //   this.resizeObserver.disconnect();
+  // }
 
   render() {
     return (
       <Host>
         <div
           class='wrapper'
-          ref={(el) => {
-            this.wrapper = el;
-          }}
+          // ref={(el) => {
+          //   this.wrapper = el;
+          // }}
         >
-          <span onClick={() => {alert('hello world')}}></span>
+          <img
+            src='https://images.unsplash.com/photo-1675789652587-5765ab02ec80?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3ODEzMTk0Mw&ixlib=rb-4.0.3&q=80&w=300'
+            alt=''
+          />
           <div>
             <h2>My Card</h2>
-            <p>My Card is a component that does something.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ipsum justo, non
+              laoreet mi hendrerit ac. Donec elementum vitae lorem id fermentum. Integer sed quam in
+              leo pretium aliquet at fermentum erat. Curabitur mollis eros non metus sagittis, quis
+              feugiat mauris convallis. Aliquam ultricies metus sollicitudin ligula porttitor
+              fringilla. Sed a pharetra ligula, nec scelerisque tellus. Nulla id neque velit. Aenean
+              ligula tellus, tempus in dolor sed, sagittis placerat magna. Phasellus ligula massa,
+              interdum eu augue non, volutpat malesuada purus. Cras volutpat viverra dapibus. Nam
+              vel mollis nulla. Aliquam a metus ultrices, feugiat libero et, lacinia augue..
+            </p>
           </div>
         </div>
       </Host>
--- src/components/my-example/my-example.tsx
+++ src/components/my-example/my-example.tsx
@@ -6,14 +6,28 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class MyExample {
-
   render() {
     return (
       <Host>
+        <h1>Container Queries</h1>
         <section>
+          <h2>card - full width</h2>
+          <my-card></my-card>
+        </section>
+        <h2>card in 1fr 2fr - Grid</h2>
+        <section class='grid'>
           <div>
-            <h1>My Example</h1>
-            <p>My Example is a component that does something.</p>
+            <h3>My Example</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia ipsum justo, non
+              laoreet mi hendrerit ac. Donec elementum vitae lorem id fermentum. Integer sed quam in
+              leo pretium aliquet at fermentum erat. Curabitur mollis eros non metus sagittis, quis
+              feugiat mauris convallis. Aliquam ultricies metus sollicitudin ligula porttitor
+              fringilla. Sed a pharetra ligula, nec scelerisque tellus. Nulla id neque velit. Aenean
+              ligula tellus, tempus in dolor sed, sagittis placerat magna. Phasellus ligula massa,
+              interdum eu augue non, volutpat malesuada purus. Cras volutpat viverra dapibus. Nam
+              vel mollis nulla. Aliquam a metus ultrices, feugiat libero et, lacinia augue.
+            </p>
           </div>
           <div>
             <my-card></my-card>
